707fe8bc2f7238cc108ab16d5e5cc6d15ef36a93
chenkh <chenkh@xym-group.com>
2020-04-15 13:10:06 +0800

    提交项目

---
+++ src/main/java/com/example/demo/config/DbConfig.java
@@ -0,0 +1,127 @@
+package com.example.demo.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.google.common.collect.Lists;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.ArrayList;
+
+import static java.lang.Boolean.TRUE;
+
+/**
+ * @author Liuhao
+ * Description:WaysDbConfigBean
+ * Date:14:30 2019/10/9
+ */
+@Configuration
+@EnableTransactionManagement
+@MapperScan("com.example.demo.dal")
+public class DbConfig {
+
+    //@Value("${ways.jdbc.url}")
+    private String url = "jdbc:mysql://10.0.2.8:3306/ways?useUnicode=true&characterEncoding=UTF-8&allowMultiQueries=true";
+
+    //@Value("${ways.jdbc.username}")
+    private String userName = "cimendev";
+
+    //@Value("${ways.jdbc.password}")
+    private String password = "123456";
+
+   // @Value("${druid.initialSize}")
+    private int initialSize = 1;
+
+    //@Value("${druid.logAbandoned}")
+    private boolean logAbandoned = TRUE;
+
+    //@Value("${druid.maxActive}")
+    private int maxActive = 50;
+
+    //@Value("${druid.maxWait}")
+    private int maxWait = 60000;
+
+    //@Value("${druid.minEvictableIdleTimeMillis}")
+    private int minEvictableIdleTimeMillis = 300000;
+
+    //@Value("${druid.minIdle}")
+    private int minIdle = 3;
+
+    //@Value("${druid.removeAbandoned}")
+    private boolean removeAbandoned = TRUE;
+
+    //@Value("${druid.removeAbandonedTimeout}")
+    private int removeAbandonedTimeout = 1800;
+
+    //@Value("${druid.timeBetweenEvictionRunsMillis}")
+    private int timeBetweenEvictionRunsMillis = 60000;
+
+    //@Value("${mybatis.mapper.location}")
+    private String mapperLocation = "classpath:mybatis/mapper/**/*.xml";
+
+    //@Value("${mybatis.config.location}")
+    private String configLocation = "classpath:mybatis/mybatis-config.xml";
+
+    @Value("${max.file.size:10}")
+    private Long maxFileSize;
+
+    @Value("${max.request.size:50}")
+    private Long maxRequestSize;
+
+    @Bean(value = "waysDataSource")
+    public DataSource getDataSource() {
+        DruidDataSource dataSource = new DruidDataSource();
+        //移除jdbc url中的amp;否则在springboot2.0+mysql8.0启动报错
+        dataSource.setUrl(this.url);
+        dataSource.setUsername(this.userName);
+        dataSource.setPassword(this.password);
+        dataSource.setInitialSize(this.initialSize);
+        dataSource.setLogAbandoned(this.logAbandoned);
+        dataSource.setMinIdle(this.minIdle);
+        dataSource.setMinEvictableIdleTimeMillis(this.minEvictableIdleTimeMillis);
+        dataSource.setMaxActive(this.maxActive);
+        dataSource.setMaxWait(this.maxWait);
+        dataSource.setRemoveAbandoned(this.removeAbandoned);
+        dataSource.setRemoveAbandonedTimeout(this.removeAbandonedTimeout);
+        dataSource.setTimeBetweenEvictionRunsMillis(this.timeBetweenEvictionRunsMillis);
+        ArrayList<String> initSql = Lists.newArrayList("set names utf8mb4;");
+        dataSource.setConnectionInitSqls(initSql);
+        return dataSource;
+    }
+
+    @Bean(value = "waysSqlSessionFactory")
+    public SqlSessionFactory sqlSessionFactory(@Qualifier(value = "waysDataSource") DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sessionFactoryBean = new SqlSessionFactoryBean();
+        sessionFactoryBean.setDataSource(dataSource);
+
+        // 配置mapper的扫描，找到所有的mapper.xml映射文件
+        Resource[] resources = new PathMatchingResourcePatternResolver().getResources(this.mapperLocation);
+        sessionFactoryBean.setMapperLocations(resources);
+
+        // 加载全局的配置文件
+        sessionFactoryBean.setConfigLocation(new DefaultResourceLoader().getResource(this.configLocation));
+        return sessionFactoryBean.getObject();
+    }
+
+    @Bean(value = "waysSqlSessionTemplate")
+    public SqlSessionTemplate sqlSessionTemplate(@Qualifier(value = "waysSqlSessionFactory") SqlSessionFactory sqlSessionFactory) {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+
+    @Bean(value = "waysDataSourceTransactionManager")
+    public DataSourceTransactionManager dataSourceTransactionManager(DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
+
+}
